8d5756416d3d5d06f9968dedcb2ee30f64cfed0b
seshusaireddy <seshusaireddy040@gmail.com>
2024-06-03 21:01:24 -0400

    Login and registration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,45 @@
-// import React from 'react';
-
-export default function App() {
+import React, {useState} from 'react';
+import Header from './pages/Header';
+import Footer from './pages/footer';
+import LoginForm from './pages/Login';
+import RegistrationForm from './pages/register';
+import Home from './pages/Home';
+import PrivateRoute from './utils/PrivateRoutes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import AlertComponent from './pages/AlertComponent';  
+function App() {
+  const [title, updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
   return (
-    <h1>App</h1>
-  )
+    <Router>
+    <div>
+      <Header title={title}/>
+        <div>
+          <Switch>
+            <Route path="/" exact={true}>
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/register">
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/login">
+              <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/home">
+              <Home/>
+            </Route>
+          </Switch>
+          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}/>
+        </div>
+      <Footer/>
+    </div>
+    
+    </Router>
+  );
 }
+
+export default App;
