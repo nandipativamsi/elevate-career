1aa51965704fa2fa42f1d62ebcb75da0111aaad6
Mihir <mp80485@gmail.com>
2024-06-04 21:46:50 -0400

    Created Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 // import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyNavbar from './components/navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import HelpCenter from './pages/HelpCenter';
 
-export default function App() {
+
+const App = () => {
   return (
-    <h1>App</h1>
-  )
-}
+    <Router>
+      <div>
+        <MyNavbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/helpcenter" element={<HelpCenter />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
