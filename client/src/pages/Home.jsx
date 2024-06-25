45197daa85f316dbca7ef23292c83167d074dbb3
Mihir <mp80485@gmail.com>
2024-06-24 20:19:31 -0400

    created reachout section in homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 // import React from 'react';
+import '../css/index.css'
 import "../css/home.css"
 import '../css/navbar.css'
-import '../css/index.css'
 import rowimg1 from '../assets/row1.png'
 import rowimg2 from '../assets/row2.png'
 import rowimg3 from '../assets/row3.png'
 import featureImg1 from '../assets/feature1.jpg'
 import featureImg2 from '../assets/feature2.jpg'
 import featureImg3 from '../assets/feature3.jpg'
+import reachoutImg from '../assets/ReachOut.png'
 
-
-import React,{ useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { ACCESS_TOKEN_NAME, API_BASE_URL } from '../constants/apiConstants';
 import axios from 'axios'
@@ -19,23 +19,16 @@ function HomePage() {
   return (
     <div>
       <header className="py-5 bg-image">
-        <div className="hero-text row justify-content-center">
-          <div className="col-lg-8 text-center">
-            <h1 className="font-weight-bold">Elevate Your Career</h1>
-            <p className="lead">
-              Discover opportunities, connect with experts, and unlock your
-              potential with Elevate Career.
-            </p>
-            <input type="text" name="search" id="hero-search" placeholder="SEARCH YOUR PASSION HERE!" />
-            <a href="#" className="my-btn btn-primary btn-lg">
-              SEARCH
-            </a>
+        <div className="hero-text">
+          <div className="text-center">
+            <h4>ONE STEP TOWARDS</h4>
+            <h1 className="bolder-text">EXCELLENCE!!</h1>
           </div>
         </div>
       </header>
 
-      <section className="py-5 bg-light">
-        <div className="row">
+      <section className="py-5">
+        <div>
           <div className="col-lg-12 text-center common-tittle">
             <h2>WHAT ARE YOUR STEPS??</h2>
           </div>
@@ -101,7 +94,29 @@ function HomePage() {
         </div>
       </section>
 
-      <footer className="py-5 bg-light">
+      <section className="py-5 reachout-container">
+        <div className="reachout-box flex-container">
+          <div className="reachout-image-container flex-container">
+            <img className="reachout-image" src={reachoutImg} alt="a girl working with laptop" />
+          </div>
+          <div className="reachout-text-container">
+            <h1>REACH OUT !</h1>
+            <p>At Elevate Career, we are here to support your professional journey every step of the way. Whether you are looking for personalized career advice, have questions about our services, or need assistance navigating your career path, we are just a click away.</p>
+            <button className="feature-button my-btn">CLICK HERE</button>
+          </div>
+        </div>
+
+        <div className="reachout-info align-text-center">
+          <div>
+            <h4>HOW TO REACH US</h4>
+          </div>
+          <div>
+            <div>contact@elevatecareer.com</div>
+            <div>+1 (123) 456-7890</div>
+          </div>
+        </div>
+      </section>
+      {/* <footer className="py-5 bg-light">
         <div className="container">
           <div className="row">
             <div className="col-lg-12 text-center">
@@ -109,31 +124,31 @@ function HomePage() {
             </div>
           </div>
         </div>
-      </footer>
+      </footer> */}
     </div>
   );
 }
 
 function Home(props) {
-    useEffect(() => {
-        axios.get(API_BASE_URL+'/user/me', { headers: { 'token': localStorage.getItem(ACCESS_TOKEN_NAME) }})
-        .then(function (response) {
-            if(response.status !== 200){
-              redirectToLogin()
-            }
-        })
-        .catch(function (error) {
+  useEffect(() => {
+    axios.get(API_BASE_URL + '/user/me', { headers: { 'token': localStorage.getItem(ACCESS_TOKEN_NAME) } })
+      .then(function (response) {
+        if (response.status !== 200) {
           redirectToLogin()
-        });
-      },[])
-    function redirectToLogin() {
+        }
+      })
+      .catch(function (error) {
+        redirectToLogin()
+      });
+  }, [])
+  function redirectToLogin() {
     props.history.push('/login');
-    }
-    return(
-        <div className="mt-2">
-            Home page content
-        </div>
-    )
+  }
+  return (
+    <div className="mt-2">
+      Home page content
+    </div>
+  )
 }
 
 export default withRouter(HomePage);
