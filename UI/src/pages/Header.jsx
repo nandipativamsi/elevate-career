bb41a9ee4fd32a9587ab5e77bd212af189f7ef7a
nvamsi2647 <vnandipati@conestogac.on.ca>
2024-07-02 19:06:31 -0400

    Fixing nav bar

--- UI/src/pages/Header.jsx
+++ UI/src/pages/Header.jsx
@@ -47,15 +47,12 @@ function Header(props) {
             <nav className="navigation">
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/jobboard">Job Board</a></li>
                     <li><a href="/help-center">Help Center</a></li>
                     {user ? (
                         <>
-                            <li><a href="/addJob">Add Job</a></li>
-                            <li><a href="/viewJobs">Jobs</a></li>
-                            <li><a href="/addEvent">Add Event</a></li>
+                            <li><a href="/addNew">Add New</a></li>
+                            <li><a href="/jobboard">Job Board</a></li>
                             <li><a href="/viewEvents">Events</a></li>
-                            <li><a href="/addResource">Add Resource</a></li>
                             <li><a href="/viewResources">Resources</a></li>
                             <li><a href="/" onClick={handleLogout}>Logout</a></li>
                             <li>Hello, {user.name}</li>
